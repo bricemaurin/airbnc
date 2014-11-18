7983c6a896094a0250255271444177eaa9a01511
Brice Maurin <brice@deux.io>
2014-11-18 10:48:36 +0100

    migration from sqlite to postgre

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141117163533) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "flats", force: true do |t|
     t.string   "title"
     t.text     "description"
@@ -46,8 +49,8 @@ ActiveRecord::Schema.define(version: 20141117163533) do
     t.integer  "flat_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["flat_id"], name: "index_users_on_flat_id"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["flat_id"], name: "index_users_on_flat_id", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
