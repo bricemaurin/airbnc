594f8a72923476ef6de3a25dfa7277f2a8332652
Brice Maurin <brice@deux.io>
2014-11-18 12:18:31 +0100

    updated models

--- app/models/flat_picture.rb
+++ app/models/flat_picture.rb
@@ -1,7 +1,7 @@
 class FlatPicture < ActiveRecord::Base
   belongs_to :flat
 
-  has_attached_file :photo,
+  has_attached_file :url,
     styles: { full: "1400x800>", banner: "1400x550#>", list: "400x250#>" }
 
   validates_attachment_content_type :photo,
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :photo,
+  has_attached_file :profile_picture,
     styles: { profile: "200x200#>", flat: "90x90#>", list: "60x60#>", nav: "30x30#>" }
 
-  validates_attachment_content_type :photo,
+  validates_attachment_content_type :profile_picture,
     content_type: /\Aimage\/.*\z/
 
   has_many :flats
