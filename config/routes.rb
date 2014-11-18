a3c3de96d0257ae8d5e8df99f170013ea6f6459f
Fleur Broca <brocafleur@gmail.com>
2014-11-18 15:57:09 +0100

    Added Facebook Connect

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   resources :flats
+
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118103810) do
+ActiveRecord::Schema.define(version: 20141118144508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,10 @@ ActiveRecord::Schema.define(version: 20141118103810) do
     t.integer  "guest_number"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "flats", ["user_id"], name: "index_flats_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                        default: "", null: false
@@ -59,15 +62,19 @@ ActiveRecord::Schema.define(version: 20141118103810) do
     t.string   "firstname"
     t.string   "lastname"
     t.text     "introduction"
-    t.integer  "flat_id"
     t.string   "profile_picture_file_name"
     t.string   "profile_picture_content_type"
     t.integer  "profile_picture_file_size"
     t.datetime "profile_picture_updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "picture"
+    t.string   "name"
+    t.string   "token"
+    t.datetime "token_expiry"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["flat_id"], name: "index_users_on_flat_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
