fc12441dbdeb5fe0bf2c859ed2183740115e7754
Fleur Broca <brocafleur@gmail.com>
2014-11-19 11:41:43 +0100

    changed nav bar

---
+++ app/assets/javascripts/signin.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $("#signup-btn").on("click", function(event){
+    $(this).parent().hide();
+    $("#signup").show();
+  });
+
+  $("#signin-btn").on("click", function(event){
+    $(this).parent().hide();
+    $("#signin").show();
+  });
+
+});
